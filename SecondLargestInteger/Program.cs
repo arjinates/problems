4a792319ff4a38e1f037332a15fd75a4dfab29bf
arjinates <arjinates@hotmail.com>
2025-02-04 21:24:22 +0300

    initial commit with SecondLargestInteger

---
+++ SecondLargestInteger/Program.cs
@@ -0,0 +1,54 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int[] array = GenerateRandomArray(10);
+        PrintArray(array);
+        int secondLargest = FindSecondLargest(array);
+        Console.WriteLine($"Second largest: {secondLargest}");
+    }
+
+    static int[] GenerateRandomArray(int size)
+    {
+        Random random = new Random();
+        int[] array = new int[size];
+        for (int i = 0; i < size; i++)
+        {
+            array[i] = random.Next(1, 101);
+        }
+        return array;
+    }
+
+    static int FindSecondLargest(int[] array)
+    {
+        if (array.Length < 2)
+        {
+            throw new ArgumentException("Array must contain at least two elements.");
+        }
+
+        int first = int.MinValue, second = int.MinValue;
+        foreach (int number in array)
+        {
+           if (number > first)
+           {
+            second = first;
+            first = number;
+           }
+           else if (number > second && number < first)
+           {
+            second = number;
+           }    
+        }
+        
+        return second;
+    }
+
+    static void PrintArray(int[] array)
+    {
+        foreach (int number in array)
+        {
+            Console.Write(number + " ");
+        }
+        Console.WriteLine();
+    }
+}
