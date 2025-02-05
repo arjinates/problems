266b45f820667150aa0a3370766095f82867233f
arjinates <arjinates@hotmail.com>
2025-02-05 22:57:55 +0300

    Add MergeSortedArray

--- Program.cs
+++ Program.cs
@@ -1,21 +1,27 @@
-using System;
-using Problems.Easy.SecondLargestInteger;
-
 namespace Problems
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            var secondLargestIntegerClass = new SecondLargestIntegerSolution();
-            int secondLargest = secondLargestIntegerClass.GenerateRandomArrayAndReturnAnswer(10);
-            Console.WriteLine($"Second largest: {secondLargest}");
+            // var secondLargestIntegerClass = new SecondLargestIntegerSolution();
+            // int secondLargest = secondLargestIntegerClass.GenerateRandomArrayAndReturnAnswer(10);
+            // Console.WriteLine($"Second largest: {secondLargest}");
 
-            var fizzBuzzSolution = new FizzBuzzSolution();
-            var responseList = fizzBuzzSolution.FizzBuzz(15);
-            foreach (string response in responseList)
+             var fizzBuzzSolution = new FizzBuzzSolution();
+            // var responseList = fizzBuzzSolution.FizzBuzz(15);
+            // foreach (string response in responseList)
+            // {
+            //     Console.WriteLine(response);
+            // }
+
+            var mergeSortedArraySolution = new MergeSortedArraySolution();
+            int[] nums1 = new int[] {1, 2, 3, 0, 0, 0};
+            int[] nums2 = new int[] {2, 5, 6};
+            mergeSortedArraySolution.Merge(nums1, 3, nums2, 3);
+            foreach (int number in nums1)
             {
-                Console.WriteLine(response);
+                Console.Write(number + " ");
             }
         }
     }
